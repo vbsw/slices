f8ae59d92b54c0c165d6effc5347c8594c861729
Vitali Baumtrok <vbsw@mailbox.org>
2020-06-06 23:14:55 +0200

    refactoring

--- insert/insert_test.go
+++ insert/insert_test.go
@@ -11,9 +11,9 @@ import (
 	"testing"
 )
 
-func TestIntN(t *testing.T) {
-	values := newValues(2, 1, 2)
+func TestIntN1(t *testing.T) {
 	insertedValues := newValues(3)
+	values := newValues(2, 1, 2)
 
 	values = IntN(values, 1, insertedValues)
 	if len(values) != 2 {
@@ -21,8 +21,12 @@ func TestIntN(t *testing.T) {
 	} else if values[0] != 1 || values[1] != 2 {
 		t.Error(values[0], values[1])
 	}
+}
 
-	values = newValues(2, 1, 2)
+func TestIntN2(t *testing.T) {
+	insertedValues := newValues(3)
+	values := newValues(2, 1, 2)
+
 	insertedValues = append(insertedValues, 10, 11)
 	values = IntN(values, 1, insertedValues)
 	if len(values) != 4 {
@@ -30,16 +34,26 @@ func TestIntN(t *testing.T) {
 	} else if values[0] != 1 || values[1] != 10 || values[2] != 11 || values[3] != 2 {
 		t.Error(values[0], values[1])
 	}
+}
 
-	values = newValues(3, 1, 2)
+func TestIntN3(t *testing.T) {
+	insertedValues := newValues(3)
+	values := newValues(3, 1, 2)
+
+	insertedValues = append(insertedValues, 10, 11)
 	values = IntN(values, 1, insertedValues)
 	if len(values) != 4 {
 		t.Error(len(values))
 	} else if values[0] != 1 || values[1] != 10 || values[2] != 11 || values[3] != 2 {
 		t.Error(values[0], values[1], values[2], values[3])
 	}
+}
 
-	values = newValues(4, 1, 2)
+func TestIntN4(t *testing.T) {
+	insertedValues := newValues(3)
+	values := newValues(4, 1, 2)
+
+	insertedValues = append(insertedValues, 10, 11)
 	values = IntN(values, 1, insertedValues)
 	if len(values) != 4 || cap(values) != 4 {
 		t.Error(len(values), cap(values))
